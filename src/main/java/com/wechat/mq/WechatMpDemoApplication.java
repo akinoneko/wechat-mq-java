a5d7b18719f485ecff570c7cac1133660840b8bc
Yuyang Zhang <zongjindulian@gmail.com>
2017-03-16 15:36:38 +0800

    init project

---
+++ src/main/java/com/wechat/mq/WechatMpDemoApplication.java
@@ -0,0 +1,16 @@
+package com.wechat.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Binary Wang
+ */
+@SpringBootApplication
+public class WechatMpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatMpDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/wechat/mq/handler/ScanHandler.java
@@ -0,0 +1,10 @@
+package com.wechat.mq.handler;
+
+/**
+ * 
+ * @author Binary Wang
+ *
+ */
+public abstract class ScanHandler extends AbstractHandler {
+
+}
